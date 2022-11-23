80d2f0939078018a6b7dadb7f0af9491b03a2aef
DmitiyOshchepkov <odo.2003@yandex.ru>
2022-11-23 23:28:21 +0300

    first commit

---
+++ Lab6/MyFunction.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal static class MyFunction
+    {
+
+        public static int ReadInt(Func<int, bool> lambda, String message = "Неверный ввод числа")
+        {
+            int num = -1;
+            bool successRead = int.TryParse(Console.ReadLine(), out num) && lambda(num);
+            while (!successRead)
+            {
+                Console.WriteLine(message);
+                successRead = int.TryParse(Console.ReadLine(), out num) && lambda(num);
+            }
+
+            return num;
+        }
+
+        public static int[] ReadIntInLine(String message = "Неверный ввод чисел")
+        {
+            while(true)
+            {
+                string[] str =  Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                int[] arr = new int[str.Length];
+                int i = 0;
+                while (i < arr.Length && int.TryParse(str[i], out arr[i])) i++;
+
+                if (i == arr.Length)
+                    return arr;
+                else
+                    Console.WriteLine(message);
+            }
+        }
+
+        public static int[] ReadArray()
+        {
+            Console.WriteLine("Введите размер");
+            int size = ReadInt((int x) => x > 0, "Неверный ввод массива");
+            int[] array = new int[size]; 
+
+            Console.WriteLine("Введите числа");
+            for (int i = 0; i < size; i++)
+                array[i] = ReadInt((int x) => true);
+
+            return array;
+        }
+
+        public static int IndexMin(int[] array)
+        {
+            if (array.Length == 0)
+                return -1;
+            int min = array[0];
+            int indMin = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if(min > array[i])
+                {
+                    min = array[i];
+                    indMin = i;
+                }
+            }
+            return indMin;
+        }
+
+        public static int IndexMax(int[] array)
+        {
+            if (array.Length == 0)
+                return -1;
+            int max = array[0];
+            int indMax = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                    indMax = i;
+                }
+            }
+            return indMax;
+        }
+
+        public static void Swap<T>(ref T x, ref T y)
+        {
+            T sup = x;
+            x = y;
+            y = sup;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (int i in array)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("1) Поменять местами\n" +
+                                "2) Пересечение\n");
+        }
+    }
+
+    enum task
+    {
+        swap = 1,
+        intersection,
+    }
+}
+
+
---
+++ Lab6/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFunction.PrintMenu();
+            Console.WriteLine("Введите задание");
+            int numTask = MyFunction.ReadInt((int x) => x > 0, "Неверный ввод задания");
+            switch(numTask)
+            {
+                case (int)task.swap:
+                    {
+                        int[] array = MyFunction.ReadIntInLine();
+                        int indMin = MyFunction.IndexMin(array);
+                        int indMax = MyFunction.IndexMax(array);
+
+                        int middle = (int)Math.Ceiling((indMax + indMin) / 2.0);
+                        for (int i = indMin; i < middle; i++)
+                            MyFunction.Swap(ref array[i], ref array[indMax - i + indMin]);
+
+
+                        MyFunction.PrintArray(array);
+                        break;
+                    }
+
+                case (int)task.intersection:
+                    {
+                        int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+                        int[] arr2 = { 2, 4, 5, 6 };
+                        int ind1 = 0;
+                        int ind2 = 0;
+
+                        int[] result = new int[Math.Max(arr1.Length, arr2.Length)];
+                        int sizeIntersection = 0;
+                        while(ind1 != arr1.Length && ind2 != arr2.Length)
+                        {
+                            if (arr1[ind1] == arr2[ind2])
+                            {
+                                result[sizeIntersection] = arr1[ind1];
+                                sizeIntersection++;
+                                ind1++;
+                                ind2++;
+                            }
+                            else if (arr1[ind1] > arr2[ind2] && ind2 < arr2.Length)
+                                ind2++;
+                            else if (arr1[ind1] < arr2[ind2] && ind1 < arr1.Length)
+                                ind1++;
+                        }
+                        Array.Resize(ref result, sizeIntersection);
+                        MyFunction.PrintArray(result);
+                        break;
+                    }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
